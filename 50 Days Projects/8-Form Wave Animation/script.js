2a85ad90967f94874d95ae484c0958cda3365219
cflores03 <brekster03@gmail.com>
2023-01-22 23:07:21 -0500

    Completion for Hiding and Unhiding Password
    Updated both input and icon tags, added cursor and added toggle for eye slash icon.

--- 50 Days Projects/8-Form Wave Animation/script.js
+++ 50 Days Projects/8-Form Wave Animation/script.js
@@ -1,8 +1,18 @@
 const labels = document.querySelectorAll('.form-control label')
+const togglePassword = document.querySelector('#togglePassword')
+const password = document.querySelector('#id_password')
 
 labels.forEach(label => {
     label.innerHTML = label.innerText
         .split('')
         .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
         .join('')
+})
+
+togglePassword.addEventListener('click', function (e) {
+  // toggle the type attribute
+  const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  // toggle the eye slash icon
+  this.classList.toggle('fa-eye-slash');
 })
